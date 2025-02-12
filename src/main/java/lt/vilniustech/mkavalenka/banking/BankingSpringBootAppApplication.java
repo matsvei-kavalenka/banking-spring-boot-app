af2de122628042dae43c733ad04733caa32cf202
matsvei-kavalenka <matsvei.kavalenka@gmail.com>
2025-02-12 17:11:28 +0200

    Init commit

---
+++ src/main/java/lt/vilniustech/mkavalenka/banking/BankingSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package lt.vilniustech.mkavalenka.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingSpringBootAppApplication.class, args);
+	}
+
+}
